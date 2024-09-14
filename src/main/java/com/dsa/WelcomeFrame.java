70b07f968a6d313ec64b2a135dbef1247a66b37e
Yearu <yeshuavenezuela0@gmail.com>
2024-09-14 15:06:33 +0800

    Adding 1st Draft of UI in Welcome Panel with Option of Admin & Customer Buttons

---
+++ src/main/java/com/dsa/WelcomeFrame.java
@@ -0,0 +1,30 @@
+package com.dsa;
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class WelcomeFrame extends JPanel {
+    private AuctionSystem system;
+
+    public WelcomeFrame(AuctionSystem system) {
+        this.system = system;
+        setLayout(new BorderLayout());
+
+        JLabel welcomeLabel = new JLabel("Welcome to Auction Online House", SwingConstants.CENTER);
+        JButton adminButton = new JButton("Admin");
+        JButton customerButton = new JButton("Customer");
+
+        // adminButton.addActionListener(e -> system.showPanel(new AdminPanel(system)));
+        // customerButton.addActionListener(e -> system.showPanel(new CustomerPanel(system)));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(adminButton);
+        buttonPanel.add(customerButton);
+
+        add(welcomeLabel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+}
