88c91246082a8b438519bb37283eac4f1524442a
Yearu <yeshuavenezuela0@gmail.com>
2024-09-17 03:03:15 +0800

    Model Class Progress but not yet Complete all Represenatation

--- src/main/java/com/dsa/customer/Bid.java
+++ src/main/java/com/dsa/customer/Bid.java
@@ -4,4 +4,44 @@ public class Bid {
 
     // Most Likely a Linked-list data Flow
 
+        // Items in Bids Initialization
+
+        private int bidId;
+        private int itemId;
+        private String bidderName;
+        private double bidPrice;
+        private boolean isWinningBid;
+    
+        // Common Contruction Worker.
+
+        public Bid(int bidId, int itemId, String bidderName, double bidPrice) {
+    
+            this.bidId = bidId;
+            this.itemId = itemId;
+            this.bidderName = bidderName;
+            this.bidPrice = bidPrice;
+            this.isWinningBid = false;
+
+        }
+
+        //Retrieve & Update ->
+
+        public int getBidId() {
+            return bidId;
+        }
+    
+        public void setBidId(int bidId) {
+            this.bidId = bidId;
+        }
+
+        // [ Same ] Continue nlng gantong Logic sa iba pa. --->
+
+
+    @Override
+    public String toString() {
+
+        return "Bid [ bidId = " + bidId + ", itemId = " + itemId + ", bidderName = " + bidderName + ", bidPrice = " + bidPrice + 
+               ", isWinningBid = " + isWinningBid + "] ";
+    }
+
 }
