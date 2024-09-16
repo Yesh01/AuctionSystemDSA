f985ea1b59e5ec6426d12b7a6009e28d72e34298
Yearu <yeshuavenezuela0@gmail.com>
2024-09-17 04:20:51 +0800

    Creating Additional Files and Packages for Organization, Also Including Auction File for Exploration. Continuation for Initialization and Guilines inf Functionality

--- src/main/java/com/dsa/admin/AuctionManager.java
+++ src/main/java/com/dsa/admin/AuctionManager.java
@@ -1,18 +1,47 @@
 package com.dsa.admin;
 
 import java.util.LinkedList;
-import java.util.List;
+
+import com.dsa.controller.AuctionController;
+import com.dsa.customer.Bid;
 
 public class AuctionManager {
 
-    //for funcs
+    // FOR FUNCTIONALITY ---->
 
-    private List<AuctionItem> auctionItems;
-    private boolean auctionActive;
-    
-    public AuctionManager() {
+    // initialization -> 
+    private AuctionController auctionController;  // Connectivity for Database
+    // private LinkedList<Auction> auctions;
+    private LinkedList<AuctionItem> auctionItems;
+
+    // 24/7hrs Contruction Worker :>
+    public AuctionManager(AuctionController auctionController) {
+        this.auctionController = auctionController;
+        // this.auctions = new LinkedList<>();
         this.auctionItems = new LinkedList<>();
-        this.auctionActive = false;
     }
+
+    public void startAuction(AuctionItem item) {
+        // TO-DO: Implement start the Auction Logic. got it? 
+    }
+
+    public void stopAuction(AuctionItem item) {
+        // TO-DO Implement stop the Auction Logic. Go and Cook already blud!
+    }
+
+    // public AuctionStatus getAuctionStatus(AuctionItem item) {
+    //     // TO-DO implementation for Status. wait. 
+    // }
+
+    public void updateAuctionItem(AuctionItem item, Bid bid) {
+        // TO-DO Implement an updatable for Item & Bid.
+    }
+
+    //Other Methods ------> Changabele and also not yet only this methods. 
+
+    
+
+
+
 
 }
